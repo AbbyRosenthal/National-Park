e925c3d03ef67f1378d03d1063d6d5ab85813ba8
Abby Rosenthal <adlundeen@gmail.com>
2022-06-08 20:51:27 -0500

    yosemite button functioning and dipslaying forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 
 var yellowstoneInfoBtn = document.getElementById("yellowstoneConfirm")
 
-
 var getYellowstoneWeather = function () {
     lat = 44.4586;
     lon = -110.8292;
@@ -43,3 +42,49 @@ function displayYellowstoneWeather(data) {
 }
 
 yellowstoneInfoBtn.addEventListener("click", getYellowstoneWeather)
+
+
+//Yosemite Button
+
+var yosemiteInfoBtn = document.getElementById("yosemiteConfirm")
+
+var getYosemiteWeather = function () {
+    lat = 37.8651;
+    lon = -119.5383;
+    var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=974e7f7c498a1d90f5f12aeb4fe7e9e2";
+    var response = fetch(weatherApi)
+
+    fetch(weatherApi).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data)
+            displayYosemiteWeather(data)
+        })
+    })
+}
+
+function displayYosemiteWeather(data) {
+    var fiveDayTempDiv = document.getElementById("park-forecast")
+    for (i = 0; i < 5; i++) {
+        var date = data.daily[i].dt
+        var humidity = data.daily[i].humidity
+        var windSpeed = data.daily[i].wind_speed
+        var temp = data.daily[i].temp.day
+        var dayDiv = document.createElement("div")
+        var dayDate = document.createElement("h3")
+        dayDate.innerHTML = dayjs.unix(date).format('M/D/YYYY');
+        var humiditiyEL = document.createElement("p")
+        humiditiyEL.innerHTML = "Humidity: " + humidity + "%"
+        var windSpeedEl = document.createElement("p")
+        windSpeedEl.innerHTML = "Wind Speed: " + windSpeed + " mph "
+        var tempEl = document.createElement("p")
+        tempEl.innerHTML = "Temp: " + temp + " °F"
+        var weatherIcon = document.createElement("img")
+        var forecastIconUrl = "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png";
+        weatherIcon.setAttribute("src", forecastIconUrl)
+        var forecastIconDescription = data.daily[i].weather[0].description;
+        dayDiv.append(dayDate, humiditiyEL, windSpeedEl, tempEl, forecastIconDescription, weatherIcon);
+        fiveDayTempDiv.appendChild(dayDiv);
+    }
+}
+
+yosemiteInfoBtn.addEventListener("click", getYosemiteWeather)
