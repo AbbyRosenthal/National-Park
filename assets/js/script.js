e0fb6aee5fea4f237ee35945fc2cd5fdc057ac7c
Abby Rosenthal <adlundeen@gmail.com>
2022-06-08 21:08:00 -0500

    grand canyon button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 //Yellowstone Button
-
 var yellowstoneInfoBtn = document.getElementById("yellowstoneConfirm")
 
 var getYellowstoneWeather = function () {
@@ -133,3 +132,48 @@ function displayGlacierWeather(data) {
 }
 
 glacierInfoBtn.addEventListener("click", getGlacierWeather)
+
+//Grand Canyon Button
+
+var grandCanyonInfoBtn = document.getElementById("grandCanyonConfirm")
+
+var getGrandCanyonWeather = function () {
+    lat = 36.0544;
+    lon = -112.1401;
+    var weatherApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=974e7f7c498a1d90f5f12aeb4fe7e9e2";
+    var response = fetch(weatherApi)
+
+    fetch(weatherApi).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data)
+            displayGrandCanyonWeather(data)
+        })
+    })
+}
+
+function displayGrandCanyonWeather(data) {
+    var fiveDayTempDiv = document.getElementById("park-forecast")
+    for (i = 0; i < 5; i++) {
+        var date = data.daily[i].dt
+        var humidity = data.daily[i].humidity
+        var windSpeed = data.daily[i].wind_speed
+        var temp = data.daily[i].temp.day
+        var dayDiv = document.createElement("div")
+        var dayDate = document.createElement("h3")
+        dayDate.innerHTML = dayjs.unix(date).format('M/D/YYYY');
+        var humiditiyEL = document.createElement("p")
+        humiditiyEL.innerHTML = "Humidity: " + humidity + "%"
+        var windSpeedEl = document.createElement("p")
+        windSpeedEl.innerHTML = "Wind Speed: " + windSpeed + " mph "
+        var tempEl = document.createElement("p")
+        tempEl.innerHTML = "Temp: " + temp + " °F"
+        var weatherIcon = document.createElement("img")
+        var forecastIconUrl = "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png";
+        weatherIcon.setAttribute("src", forecastIconUrl)
+        var forecastIconDescription = data.daily[i].weather[0].description;
+        dayDiv.append(dayDate, humiditiyEL, windSpeedEl, tempEl, forecastIconDescription, weatherIcon);
+        fiveDayTempDiv.appendChild(dayDiv);
+    }
+}
+
+grandCanyonInfoBtn.addEventListener("click", getGrandCanyonWeather)
